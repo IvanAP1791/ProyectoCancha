ec6186b2e470c54a7cf857584c7ef7c85c38d780
IvanAP1791 <ivan.ap9117@gmail.com>
2019-04-03 21:46:23 -0300

    Commit inicial

--- Dominio/Encargado.cs
+++ Dominio/Encargado.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProyectoCanchaPaddle
+namespace Dominio
 {
-    public class Class1
+    public class Encargado
     {
     }
 }
