9a51178710686fa18f30f5d2eda1a76d62ce37cf
IvanAP1791 <ivan.ap9117@gmail.com>
2019-04-06 20:57:26 -0300

    Agregados los atributos de la clase Persona

--- Dominio/Persona.cs
+++ Dominio/Persona.cs
@@ -8,5 +8,20 @@ namespace Dominio
 {
     public class Persona
     {
+        string nombre;
+        string apellido;
+        int edad;
+        string documento;
+        string direccion;
+        string correoElectronico;
+        int telefono;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Documento { get => documento; set => documento = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string CorreoElectronico { get => correoElectronico; set => correoElectronico = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
     }
 }
